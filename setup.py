faf45d81c17b7394fc2f8b0c417a7bb3f5c11f57
David Buxton <david@gasmark6.com>
2016-02-03 21:03:03 +0000

    Teach setuptools how to run the tests, instead of a separate test runner.
    The setup.py includes test dependencies depending on your Python version.
    This may come back to bite me later, but I like the idea of letting
    setuptools run the tests, makes it easier for you to run the tests without
    needing tox.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
+import sys
 
 import electricity
+
+
+# Tell Python27 we need mock to run tests with `python setup.py test`
+tests_require = ['mock'] if sys.version_info.major < 3 else []
 
 
 setup(
@@ -9,9 +14,14 @@ setup(
     description='A static site generator.',
     author='David Buxton',
     author_email='david@gasmark6.com',
-    packages=['electricity'],
+    packages=[
+        'electricity',
+        'electricity.tests',
+    ],
     install_requires=[
         'PyYAML>=3.08',
         'six',
     ],
+    test_suite='electricity.tests',
+    tests_require=tests_require,
 )
